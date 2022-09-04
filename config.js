74ef78be48ebda759c71a068094209f95ea9dfdc
zakixd12 <zakiarlop1927@gmail.com>
2022-09-04 18:38:19 +0700

    tes uploud ya

---
+++ config.js
@@ -0,0 +1,55 @@
+/**
+   * Create By Dika Ardnt.
+   * Contact Me on wa.me/6288292024190
+   * Follow https://github.com/DikaArdnt
+*/
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+global.APIs = {
+	zenz: 'https://zenzapis.xyz',
+}
+
+// Udah Free Apikey Jadi Klo Mau Ubah Silahkan
+global.APIKeys = {
+	'https://zenzapis.xyz': 'sonelstore',
+}
+// Other
+global.owner = ['6289506219231']
+global.premium = ['6289506219231']
+global.ownername = 'Jackytron'
+global.packname = 'Jackytron?'
+global.author = 'WhatsApp 👿'
+global.sessionName = 'Fax'
+global.linkgrupss = 'https://wa.me/6289506219231'
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = '⭔'
+global.mess = {
+    success: 'Donee',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    owner: 'Fitur Khusus Owner Bot',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: 'Espere a que su solicitud sea procesada',
+    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+}
+global.limitawal = {
+    premium: "Infinity",
+    free: 100
+}
+global.thumb = fs.readFileSync('./lib/thum.jpg')
+global.visoka = { url: 'https://telegra.ph/file/15209657f9d4f59c7ca1e.mp4' }
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
